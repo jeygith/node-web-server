261678d702144a83a6c863ea6bc0603017ad38fd
githire <geoffrey.mwaura@openworld.co.ke>
2019-06-07 18:16:42 +0300

    middleware intro

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 
 hbs.registerPartials(__dirname + '/views/__partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+
+    let now = new Date().toString();
+
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+
+    fs.appendFile('server.log', log + "\n", (err) => {
+        if (err) {
+            console.log("unable to append to server.log");
+        }
+    })
+    next();
+
+});
+
+/*app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});*/
+
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper('getCurrentYear', () => {
